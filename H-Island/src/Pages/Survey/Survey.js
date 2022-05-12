43ff67b109fccd9c60ad0a557e4b955ec14a6d64
unknown <hajmal05@uw.edu>
2022-05-12 16:04:13 -0700

    created survey questions 1-5

--- H-Island/src/Pages/Survey/Survey.js
+++ H-Island/src/Pages/Survey/Survey.js
@@ -10,14 +10,65 @@ import { Survey } from "survey-react-ui";
 StylesManager.applyTheme("modern");
 
 const surveyJson = {
-  elements: [{
-    name: "FirstName",
-    title: "Enter your first name:",
-    type: "text"
+  pages: [{
+      elements: [{
+          name: "Question 1",
+          title: "Do you have children (under the age the of 12) traveling with you?",
+          type: "radiogroup",
+          choices: [
+              { value: 1, text: "Yes, I have children traveling with me" },
+              { value: 2, text: "No, I don't have children traveling with me" }
+          ],
+          isRequired: true
+      }]
   }, {
-    name: "LastName",
-    title: "Enter your last name:",
-    type: "text"
+      elements: [{
+          name: "Question 2",
+          title: "What time of the year would you like to visit?",
+          type: "radiogroup",
+          choices: [
+            {value: 1, text: "September to November"},
+            {value: 2, text: "December to February"},
+            {value: 3, text: "March to May"},
+            {value: 4, text: "June to August"}
+          ],
+          isRequired: true
+      }],
+  }, {
+      elements: [{
+          name: "Question 3",
+          title: "What type of activities do you enjoy?",
+          type: "radiogroup",
+          choices: [
+            {value: 1, text: "Low-intensity (beach-bathing, sight-seeing, etc)"},
+            {value: 2, text: "Medium-intensity (biking, recreational sports, etc)"},
+            {value: 3, text: "High-intensity (hiking, running, swiming, etc)"},
+            {value: 4, text: "Extreme-intensity (sky-diving, cliff jumping, ziplining, etc)"}
+          ],
+          isRequired: true
+      }],
+  }, {
+      elements: [{
+          name: "Question 4",
+          title: "What are you most interesting in exploring?",
+          type: "radiogroup",
+          choices: [
+            {value: 1, text: "Mountanoius"},
+            {value: 2, text: "Beaches"},
+            {value: 3, text: "Lakes"},
+            {value: 4, text: "City"}
+          ],
+      }],
+  }, {
+      elements: [{
+        name: "Question 5",
+          title: "How much money are you willing to spend?",
+          type: "radiogroup",
+          choices: [
+            {value: 1, text: "My budget is tight"},
+            {value: 2, text: "My vaction fund is full"}
+          ],
+      }]
   }]
 };
 
