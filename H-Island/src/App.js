a3f0ee9137c9784699201380c06b1056905c5610
unknown <hajmal05@uw.edu>
2022-05-16 09:47:07 -0700

    modified routing

--- H-Island/src/App.js
+++ H-Island/src/App.js
@@ -11,9 +11,10 @@ export default function App() {
   return (
     <div>
       <Nav/>
-        <Routes>
+      <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/survey" element={<Survey />} />
+          <Route path="/result" element={<Result />} />
         </Routes>
 
 
