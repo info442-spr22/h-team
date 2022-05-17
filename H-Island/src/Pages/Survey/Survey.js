a1707e09c4dc82fccaaf4128e83918d82dc6fe79
LouisXO <qleng@uw.edu>
2022-05-17 00:40:00 -0700

    Tried Multi Choice but failed, added the algorithm for select the island

--- H-Island/src/Pages/Survey/Survey.js
+++ H-Island/src/Pages/Survey/Survey.js
@@ -4,141 +4,370 @@ import {Link} from "react-router-dom";
 import 'survey-core/modern.min.css';
 import { StylesManager, Model } from "survey-core";
 import { Survey } from "survey-react-ui";
+import island_data from "../../data/islands.json";
 
 StylesManager.applyTheme("modern");
 
 const surveyJson = {
   pages: [{
       elements: [{
-          name: "Question 1",
+          name: "timeToVisit",
           title: "What time of the year would you like to visit?",
           type: "radiogroup",
           choices: [
-            {value: 1, text: "September to November"},
-            {value: 2, text: "December to February"},
-            {value: 3, text: "March to May"},
-            {value: 4, text: "June to August"}
+            {value: "March to May", text: "March to May"},
+            {value: "June to August", text: "June to August"},
+            {value: "September to November", text: "September to November"},
+            {value: "December to February", text: "December to February"},
           ],
           isRequired: true
       }]
   }, {
       elements: [{
-          name: "Question 2",
+          name: "budget",
           title: "How much money are you willing to spend?",
           type: "radiogroup",
           choices: [
-            {value: 1, text: "My budget is tight"},
-            {value: 2, text: "My vaction fund is full"}
+            {value: "Tight", text: "My budget is tight"},
+            {value: "Full", text: "My vacation fund is full"}
           ],
           isRequired: true
       }],
   }, {
       elements: [{
-          name: "Question 3",
+          name: "region",
           title: "What region are you most interested in visiting?",
           type: "radiogroup",
           choices: [
-            {value: 1, text: "Caribbean"},
-            {value: 2, text: "Pacific Ocean"},
-            {value: 3, text: "Atlantic Ocean"},
-            {value: 4, text: "Indian Ocean"}
+            {value: "Caribbean", text: "Caribbean"},
+            {value: "Pacific Ocean", text: "Pacific Ocean"},
+            {value: "Atlantic Ocean", text: "Atlantic Ocean"},
+            {value: "Indian Ocean", text: "Indian Ocean"}
           ],
           isRequired: true
       }],
   }, {
       elements: [{
-          name: "Question 4",
+          name: "activityIntensity",
           title: "What type of activities do you enjoy?",
           type: "radiogroup",
           choices: [
-            {value: 1, text: "Low-intensity (beach-bathing, sight-seeing, etc)"},
-            {value: 2, text: "Medium-intensity (biking, recreational sports, etc)"},
-            {value: 3, text: "High-intensity (hiking, running, swiming, etc)"},
-            {value: 4, text: "Extreme-intensity (sky-diving, cliff jumping, ziplining, etc)"}
+            {value: "Low", text: "Low-intensity (beach-bathing, sight-seeing, etc)"},
+            {value: "Medium", text: "Medium-intensity (biking, recreational sports, etc)"},
+            {value: "High", text: "High-intensity (hiking, running, swiming, etc)"},
+            {value: "Extreme", text: "Extreme-intensity (sky-diving, cliff jumping, ziplining, etc)"}
           ],
           isRequired: true
       }],
   }, {
       elements: [{
-          name: "Question 5",
+          name: "exploration",
           title: "What are you most interested in exploring?",
           type: "radiogroup",
           choices: [
-            {value: 1, text: "Mountainous"},
-            {value: 2, text: "Beaches"},
-            {value: 3, text: "Lakes"},
-            {value: 4, text: "City"}
+            {value: "Mountainous", text: "Mountainous"},
+            {value: "Beaches", text: "Beaches"},
+            {value: "Lakes", text: "Lakes"},
+            {value: "City", text: "City"}
           ],
       }]
   }, {
       elements: [{
-        name: "Question 6",
+        name: "otherVaccines",
           title: "Some countries recommend tourist to obtain specific vaccines before visiting. Are you willing to to get more vaccines to protect yourself against unfamiliar diseases?",
           type: "radiogroup",
           choices: [
-            {value: 1, text: "I am willing"},
-            {value: 2, text: "I am not willing"}
+            {value: "Yes", text: "I am willing"},
+            {value: "No", text: "I am not willing"}
           ],
           isRequired: true
       }]
   }, {
       elements: [{
-        name: "Question 7",
+        name: "covidVaxed",
           title: "Are you vaccinated against COVID-19?",
           type: "radiogroup",
           choices: [
-            {value: 1, text: "I am not vaccinated, but will get tested"},
-            {value: 2, text: "I am vaccinated, but only have one dose"},
-            {value: 3, text: "I am vaccinated, but with only two doses"},
-            {value: 4, text: "I am fully vaccinated with all doses"}
+            {value: "No", text: "I am not vaccinated but will get tested"},
+            {value: "Yes", text: "I am vaccinated (one, two, or more vaccine doses"}
           ],
           isRequired: true
       }]
   }, {
       elements: [{
-          name: "Question 8",
+          name: "bestWayToGetAround",
           title: "What means of tranport do you prefer to use while at your travel destination?",
           type: "radiogroup",
           choices: [
-            {value: 1, text: "Personal Rental Car"},
-            {value: 2, text: "Rideshare services"},
-            {value: 3, text: "Walking"},
-            {value: 4, text: "Public Tranportaion"}
+            {value: "Car", text: "Personal Rental Car"},
+            {value: "RideShare", text: "Rideshare services"},
+            {value: "Walking", text: "Walking"},
+            {value: "PublicTransport", text: "Public Tranportaion"}
           ],
           isRequired: true
       }]
   }, {
       elements: [{
-          name: "Question 9",
+          name: "children",
           title: "Do you have children (under the age of 12) traveling with you?",
           type: "radiogroup",
           choices: [
-            {value: 1, text: "Yes, I have children traveling with me"},
-            {value: 2, text: "No, I do not have any children traveling with me"}
+            {value: "Yes", text: "Yes, I have children traveling with me"},
+            {value: "No", text: "No, I do not have any children traveling with me"}
           ],
           isRequired: true
       }]
   }, {
       elements: [{
-          name: "Question 10",
-          title: "SIDS countries, have people that speak various languages, do you have a preferance towards a language spoken at your travel destination??",
+          name: "languages",
+          title: "SIDS countries, have people that speak various languages, do you have a preference towards a language spoken at your travel destination??",
           type: "radiogroup",
           choices: [
-            {value: 1, text: "I would prefer English as one of the languages"},
-            {value: 2, text: "I would prefer Spanish as one of the languages"},
-            {value: 3, text: "I would prefer French as one of the languages"},
-            {value: 4, text: "No preferance"}
+            {value: "English", text: "I would prefer English as one of the languages"},
+            {value: "Spanish", text: "I would prefer Spanish as one of the languages"},
+            {value: "French", text: "I would prefer French as one of the languages"},
+            {value: "", text: "No preferance"}
           ],
       }]
-  }]
-};
+  }]};
+
+// Multi choice version
+//   pages: [{
+//     elements: [{
+//         name: "timeToVisit",
+//         title: "What time of the year would you like to visit?",
+//         type: "checkbox",
+//         choices: [
+//           {value: "March to May", text: "March to May"},
+//           {value: "June to August", text: "June to August"},
+//           {value: "September to November", text: "September to November"},
+//           {value: "December to February", text: "December to February"},
+//         ],
+//         isRequired: true
+//     }]
+// }, {
+//     elements: [{
+//         name: "budget",
+//         title: "How much money are you willing to spend?",
+//         type: "radiogroup",
+//         choices: [
+//           {value: "Tight", text: "My budget is tight"},
+//           {value: "Full", text: "My vacation fund is full"}
+//         ],
+//         isRequired: true
+//     }],
+// }, {
+//     elements: [{
+//         name: "region",
+//         title: "What region are you most interested in visiting?",
+//         type: "checkbox",
+//         choices: [
+//           {value: "Caribbean", text: "Caribbean"},
+//           {value: "Pacific Ocean", text: "Pacific Ocean"},
+//           {value: "Atlantic Ocean", text: "Atlantic Ocean"},
+//           {value: "Indian Ocean", text: "Indian Ocean"}
+//         ],
+//         isRequired: true
+//     }],
+// }, {
+//     elements: [{
+//         name: "activityIntensity",
+//         title: "What type of activities do you enjoy?",
+//         type: "checkbox",
+//         choices: [
+//           {value: "Low", text: "Low-intensity (beach-bathing, sight-seeing, etc)"},
+//           {value: "Medium", text: "Medium-intensity (biking, recreational sports, etc)"},
+//           {value: "High", text: "High-intensity (hiking, running, swiming, etc)"},
+//           {value: "Extreme", text: "Extreme-intensity (sky-diving, cliff jumping, ziplining, etc)"}
+//         ],
+//         isRequired: true
+//     }],
+// }, {
+//     elements: [{
+//         name: "exploration",
+//         title: "What are you most interested in exploring?",
+//         type: "checkbox",
+//         choices: [
+//           {value: "Mountainous", text: "Mountainous"},
+//           {value: "Beaches", text: "Beaches"},
+//           {value: "Lakes", text: "Lakes"},
+//           {value: "City", text: "City"}
+//         ],
+//     }]
+// }, {
+//     elements: [{
+//       name: "otherVaccines",
+//         title: "Some countries recommend tourist to obtain specific vaccines before visiting. Are you willing to to get more vaccines to protect yourself against unfamiliar diseases?",
+//         type: "radiogroup",
+//         choices: [
+//           {value: "Yes", text: "I am willing"},
+//           {value: "No", text: "I am not willing"}
+//         ],
+//         isRequired: true
+//     }]
+// }, {
+//     elements: [{
+//       name: "covidVaxed",
+//         title: "Are you vaccinated against COVID-19?",
+//         type: "radiogroup",
+//         choices: [
+//           {value: "No", text: "I am not vaccinated but will get tested"},
+//           {value: "Yes", text: "I am vaccinated (one, two, or more vaccine doses"}
+//         ],
+//         isRequired: true
+//     }]
+// }, {
+//     elements: [{
+//         name: "bestWayToGetAround",
+//         title: "What means of tranport do you prefer to use while at your travel destination?",
+//         type: "checkbox",
+//         choices: [
+//           {value: "Car", text: "Personal Rental Car"},
+//           {value: "RideShare", text: "Rideshare services"},
+//           {value: "Walking", text: "Walking"},
+//           {value: "PublicTransport", text: "Public Tranportaion"}
+//         ],
+//         isRequired: true
+//     }]
+// }, {
+//     elements: [{
+//         name: "children",
+//         title: "Do you have children (under the age of 12) traveling with you?",
+//         type: "radiogroup",
+//         choices: [
+//           {value: "Yes", text: "Yes, I have children traveling with me"},
+//           {value: "No", text: "No, I do not have any children traveling with me"}
+//         ],
+//         isRequired: true
+//     }]
+// }, {
+//     elements: [{
+//         name: "languages",
+//         title: "SIDS countries, have people that speak various languages, do you have a preference towards a language spoken at your travel destination??",
+//         type: "radiogroup",
+//         choices: [
+//           {value: "English", text: "I would prefer English as one of the languages"},
+//           {value: "Spanish", text: "I would prefer Spanish as one of the languages"},
+//           {value: "French", text: "I would prefer French as one of the languages"},
+//           {value: "", text: "No preferance"}
+//         ],
+//     }]
+// }]};
+
+
+//   pages: [{
+//     elements: [{
+//         name: "timeToVisit",
+//         title: "What time of the year would you like to visit?",
+//         type: "radiogroup",
+//         choices: [
+//           {value: "September to November", text: "September to November"},
+//           {value: "December to February", text: "December to February"},
+//           {value: "March to May", text: "March to May"},
+//           {value: "June to August", text: "June to August"}
+//         ],
+//         isRequired: true
+//     }]
+// }, {
+//     elements: [{
+//         name: "budget",
+//         title: "How much money are you willing to spend?",
+//         type: "radiogroup",
+//         choices: [
+//           {value: "Tight", text: "My budget is tight"},
+//           {value: "Full", text: "My vacation fund is full"}
+//         ],
+//         isRequired: true
+//     }]
+//   }, {
+//     elements: [{
+//         name: "languages",
+//         title: "SIDS countries, have people that speak various languages, do you have a preferance towards a language spoken at your travel destination??",
+//         type: "radiogroup",
+//         choices: [
+//           {value: "English", text: "I would prefer English as one of the languages"},
+//           {value: "Spanish", text: "I would prefer Spanish as one of the languages"},
+//           {value: "French", text: "I would prefer French as one of the languages"},
+//           {value: "Other", text: "No preferance"}
+//         ],
+//     }]
+// }]
+// };
+
+// pages: [{
+//   elements: [{
+//       name: "timeToVisit",
+//       title: "What time of the year would you like to visit?",
+//       type: "checkbox",
+//       choices: [
+//         {value: "September to November", text: "September to November"},
+//         {value: "December to February", text: "December to February"},
+//         {value: "March to May", text: "March to May"},
+//         {value: "June to August", text: "June to August"}
+//       ],
+//       isRequired: true
+//   }]
+// }, {
+//   elements: [{
+//       name: "budget",
+//       title: "How much money are you willing to spend?",
+//       type: "radiogroup",
+//       choices: [
+//         {value: "Tight", text: "My budget is tight"},
+//         {value: "Full", text: "My vacation fund is full"}
+//       ],
+//       isRequired: true
+//   }]
+// }]};
+
+function PickIslands(prop) {
+  const surveyResult = prop
+  console.log(island_data)
+  console.log(surveyResult)
+
+
+  function filterBy(list, criteria) {
+    return list.filter(island =>
+      Object.keys(criteria).every(key =>
+        // {for (var keys in key) {
+          island[key].includes(criteria[key][0])
+        // }}
+      )
+    );
+  }
+
+  //Tried Multi Choice
+//  function filterBy(list, criteria) {
+//     return list.filter(island =>
+//       Object.keys(criteria).every(key =>
+//         // {for (var keys in key) {
+//           criteria[key].forEach(element => {
+//             island[key].includes(element)
+//           })
+
+//           // island[key].includes(criteria[key][0])
+//         // }}
+//       )
+//     );
+//   }
+
+console.log(filterBy(island_data,surveyResult));
+}
+
+
+
+
 
 export default function SurveyPage() {
   const survey = new Model(surveyJson);
 
   const alertResults = useCallback((sender) => {
-    const results = JSON.stringify(sender.data);
-    alert(results);
+    const resultsArray = Object.entries(sender.data)
+    const results = sender.data;
+
+
+    PickIslands(results)
+    alert(resultsArray);
   }, []);
 
   survey.onComplete.add(alertResults);
@@ -149,3 +378,6 @@ export default function SurveyPage() {
       </div>
     );
 }
+
+
+//{"Question 1":"September to November","Question 2":"Full","Question 3":"Indian Ocean","Question 4":"High","Question 5":"Beaches","Question 6":"No","Question 7":2,"Question 8":"Walking","Question 9":"No","Question 10":"French"}
