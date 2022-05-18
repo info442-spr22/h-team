65d2e4a0dff58e55299117297014ae550e899b3c
unknown <hajmal05@uw.edu>
2022-05-17 19:58:40 -0700

    edited css for survey page

--- H-Island/src/Pages/Survey/Survey.js
+++ H-Island/src/Pages/Survey/Survey.js
@@ -373,7 +373,7 @@ export default function SurveyPage() {
   survey.onComplete.add(alertResults);
 
     return (
-      <div className='surveyMain'>
+      <div className>
         <div>
           <Survey model={survey} />
         </div>
